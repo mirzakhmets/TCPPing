21dde06eb8d6f04a37915ae9f2d076fdb4be90e6
mirzakhmets <mirzakhmets@icloud.com>
2024-09-19 14:37:37 +0500

    Added trial

--- ping.cpp
+++ ping.cpp
@@ -22,7 +22,60 @@ int PrUsage(char * sPath) {
 	printf("Example: %s localhost 110 10 -ct -t100", sPath);
 	return (0);
 }
+
+bool IsRegistered() {
+	DWORD value = -1;
+	DWORD valueSize = sizeof(value);
+	
+	if (RegGetValue (HKEY_CURRENT_USER, "Software\\OVG-Developers", "Registered", REG_DWORD, NULL, &value, &valueSize) == ERROR_SUCCESS) {
+		return true;
+	}
+	
+	return value != -1;
+}
+
+void CheckRuns() {
+	DWORD value = -1;
+	DWORD valueSize = sizeof(value);
+	DWORD valueType = REG_DWORD;
+	
+	HKEY hKey;
+	DWORD dwDisposition;
+	
+	RegCreateKeyEx(HKEY_CURRENT_USER, "Software\\OVG-Developers", 0, NULL, 0, 0, NULL, &hKey, &dwDisposition);
+	
+	if(ERROR_SUCCESS != RegOpenKeyEx(HKEY_CURRENT_USER, "Software\\OVG-Developers", 0, KEY_SET_VALUE, &hKey)) 
+	{
+	}
+	
+	RegGetValue(HKEY_CURRENT_USER, "Software\\OVG-Developers", "Runs", REG_DWORD, NULL, &value, &valueSize);
+
+	RegCloseKey(hKey);
+	
+	value = value + 1;
+	
+	if (value > 10) {
+		puts ("Number of runs expired. Please purchase the program (visit site https://ovg-developers.mystrikingly.com/).");
+		
+		exit(0);
+	}
+	
+	valueSize = sizeof(value);
+	
+	DWORD dwValue = (DWORD) value;	
+	
+	RegOpenKeyEx(HKEY_CURRENT_USER, "Software\\OVG-Developers", 0, KEY_SET_VALUE, &hKey);
+	
+	RegSetValueEx(hKey, "Runs", 0, REG_DWORD, (const BYTE*) &dwValue, sizeof (dwValue));
+	
+	RegCloseKey(hKey);
+}
+
 int main(int argc, char ** argv) {
+	if (!IsRegistered()) {
+		CheckRuns();
+	}
+	
 	char sBuffer[256];
 	int iRecvd, iSent, iTotalTimeRecvd, iTotalTimeSent;
 	int iStats[8];
